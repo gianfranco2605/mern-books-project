9a48bac313f0ef2dc7c6be5400948e365822ec0e
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-09-10 12:08:04 +0200

    front-back connected

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,32 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBUrl } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
 //Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handling CORS POLICY
+// Option 1: Allow all origins with default of cors(*)
+app.use(cors());
+// Option 2
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+// })
+// );
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome");
 })
 
-//Route to Save a new Book
-app.post('/books', async ( request, response ) => {
-    try {
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: "Send all required fields: title, author and publishYear"
-            })
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear
-        }
-
-        const book = await Book.create(newBook)
-
-        return response.status(201).send(book);
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBUrl)
